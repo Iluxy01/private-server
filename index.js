e45ae0fb7f75533dba5f82772503dbc814372600
Iluxy01 <imatysak11@gmail.com>
2026-04-10 22:35:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ const { initDB } = require('./db');
 const authRouter = require('./routes/auth');
 const usersRouter = require('./routes/users');
 const chatsRouter = require('./routes/chats');
-=======
-const authRouter = require('./auth');
-const usersRouter = require('./users');
-const chatsRouter = require('./chats');
->>>>>>> 5d4c91603c3039364a0a5d70b66abf16c774ce07
 const { setupWebSocket } = require('./websocket');
 
 const app = express();
